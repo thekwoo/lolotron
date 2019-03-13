89f8448405b813344421bffa21ecb4db0e5ca29a
Kevin Woo <kewoo@microsoft.com>
2019-03-13 15:10:58 -0700

    Added tracking of signups, deletion of original messag,e and use of templating to generate the message

--- main.py
+++ main.py
@@ -5,15 +5,60 @@ import discord.ext.commands as dxc
 import json
 import sys
 import time
+from typing import Any,Dict,List,Tuple
 
 # We need to create the client early so that we can override a lot of the functions
 # internally. This version is the Bot version so we have access to command parsing
 client = dxc.Bot('%', description='')
 
 # For the RSVP systme we need to track events
-rsvpEntry = namedtuple('rsvpEntry', ['user', 'timeStamp'])
-rsvpEvent = namedtuple('rsvpEvent', ['owner', 'message', 'rsvps', 'expire'])
+rsvpEntry = namedtuple('rsvpEntry', ['user', 'timeStamp', 'valid'])
+rsvpEvent = namedtuple('rsvpEvent', ['owner', 'message', 'rsvps', 'expire', 'id'])
 rsvpTracker = {}
+
+rsvpEmoji = '\U0001F64B'
+rsvpTemplateMessageBody = \
+'''
+Posted by: {}
+{}
+
+Please react to this message with :raising_hand: to join.
+Removing your reaction will lose your spot in the queue.
+
+Signup:
+'''
+
+rsvpTemplateMessageFoot = \
+'''
+Additional Options:
+rsvp_delete <SystemID> - to delete this (owner only)
+rsvp_audit  <SystemID> - to show entire history
+rsvp_ext    <SystemID)
+SystemID: {}
+Expiration Time: {}
+'''
+
+'''
+Helper function that generates the message
+'''
+def rsvpMsgGenerator(event:rsvpEvent) -> str:
+    # Create the header
+    msg = rsvpTemplateMessageBody.format(event.owner.display_name, event.message)
+
+    # Iterate through the RSVP list in order, skipping entires that were cancelled
+    # Also keep a counter for enumeration, but start with 1 index for non-programmers
+    cnt = 1
+    for r in event.rsvps:
+        if r.valid:
+            msg += '{} - {}\n'.format(cnt, r.user.display_name)
+
+
+    # Append the footer information
+    # TODO: The expiration time should be something better than the float, we should format it
+    msg += rsvpTemplateMessageFoot.format(event.id, event.expire)
+
+    return msg
+
 
 @client.event
 async def on_connect():
@@ -41,16 +86,24 @@ async def rsvp(ctx, *, arg):
     owner = ctx.author
     print('The owner of this message is: {}'.format(owner))
 
-    msg = await ctx.channel.send('This is an example of me modifying the message.\n' + msgBody)
 
-    firstEntry = rsvpEntry(owner, time.time())
-    rsvpList = [firstEntry]
-
+    # We need to create a message and send it to get a messageID, since we use the messageID as the identifier
+    # We will edit the actual content later
+    msg = await ctx.channel.send('Preparing an RSVP message...')
     print('The message I just sent has ID {:d}'.format(msg.id))
 
-    event = rsvpEvent(owner, msgBody, rsvpList, None)
+    # Finish setting up the RSVP Event Object
+    firstEntry = rsvpEntry(owner, time.time(), True)
+    rsvpList = [firstEntry]
+    event = rsvpEvent(owner, msgBody, rsvpList, 0.0, msg.id)
     rsvpTracker[msg.id] = event
 
+    # Update the RSVP Message from the bot
+    msgTxt = rsvpMsgGenerator(event)
+    await msg.edit(content=msgTxt)
+
+    # Delete the original message now that we're done parsing it
+    await ctx.message.delete()
 
 #@client.event
 #async def on_message(message):
@@ -74,8 +127,10 @@ async def on_reaction_add(reaction, user):
     else:
         event = rsvpTracker[msgId]
 
-    # Grab information about the event
-    reactEmjoi = reaction.emoji
+    # Check that this is the correct
+    if rsvpEmoji != reaction.emoji:
+        print('reacted emjoi doesnt match expected. got {}'.format(reaction.emoji))
+        return
 
     # Check if the user is already in the list, this should really just be an edge case for the owner
     for r in event.rsvps:
@@ -83,11 +138,11 @@ async def on_reaction_add(reaction, user):
             return
 
     # Add RSVP to the list
-    newEntry = rsvpEntry(user, time.time())
+    newEntry = rsvpEntry(user, time.time(), True)
     event.rsvps.append(newEntry)
 
-    print('New event list:')
-    print(event.rsvps)
+    msgTxt = rsvpMsgGenerator(event)
+    await reaction.message.edit(content=msgTxt)
 
 ###############################################################################
 # Load General Settings
